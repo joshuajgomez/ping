7cee0d2b5b76e25a33cba08af16c24ae348882a6
Joshua Gomez <joshuajgomez@gmail.com>
2024-02-24 23:14:19 +0530

    Move navigation logic to separate file

--- app/src/main/java/com/joshgm3z/ping/HomeActivity.kt
+++ app/src/main/java/com/joshgm3z/ping/HomeActivity.kt
@@ -4,24 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.joshgm3z.ping.service.PingService
-import com.joshgm3z.ping.viewmodels.ChatViewModel
-import com.joshgm3z.ping.ui.chat.ChatScreenContainer
-import com.joshgm3z.ping.ui.frx.SignInContainer
-import com.joshgm3z.ping.viewmodels.SignInViewModel
-import com.joshgm3z.ping.ui.home.HomeScreenContainer
-import com.joshgm3z.ping.viewmodels.HomeViewModel
-import com.joshgm3z.ping.ui.search.SearchContainer
 import com.joshgm3z.ping.ui.theme.PingTheme
 import com.joshgm3z.ping.utils.Logger
 import com.joshgm3z.ping.utils.DataStoreUtil
@@ -29,14 +18,8 @@ import org.koin.android.ext.android.get
 
 class HomeActivity : ComponentActivity() {
 
-    private val navSignIn = "signin_screen"
-    private val navHome = "home_screen"
-    private val navSearch = "search_screen"
-    private val navChat = "chat_screen"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FirebaseAnalytics.getInstance(this).logEvent("MainActivity_onCreate", Bundle())
         if (!PingService.isRunning) {
             Logger.debug("PingService.isRunning = [${PingService.isRunning}]")
             val intent = Intent(this, PingService.javaClass)
@@ -48,57 +31,17 @@ class HomeActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-
                     val dataStore: DataStoreUtil = get()
                     val isUserSignedIn = dataStore.isUserSignedIn()
-
-                    NavHost(
+                    val navController = rememberNavController()
+                    PingAppContainer(
                         navController = navController,
-                        startDestination = if (isUserSignedIn) navHome else navSignIn
-                    ) {
-
-                        composable(navSignIn) {
-                            Logger.warn("signIn")
-                            SignInContainer(
-                                signInViewModel = get(),
-                                goToHome = { navController.navigate(navHome) }
-                            )
-                        }
-
-                        composable(navHome) {
-                            Logger.warn("navHome")
-                            HomeScreenContainer(
-                                homeViewModel = get(),
-                                onSearchClick = { navController.navigate(navSearch) },
-                                onChatClick = { navController.navigate("$navChat/${it.otherGuy.docId}") },
-                            )
-                        }
-
-                        val chatViewModel: ChatViewModel = get()
-                        composable("$navChat/{userId}") {
-                            val userId = it.arguments?.getString("userId")
-                            LaunchedEffect(key1 = userId) {
-                                Logger.warn("navChat setUser = [$userId]")
-                                chatViewModel.setUser(userId!!)
-                            }
-                            Logger.warn("navChat userId = [$userId]")
-                            ChatScreenContainer(
-                                chatViewModel = chatViewModel,
-                                onBackClick = { navController.navigate(navHome) }
-                            )
-                        }
-
-                        composable(navSearch) {
-                            Logger.warn("navSearch")
-                            SearchContainer(
-                                searchViewModel = get(),
-                                onSearchItemClick = { navController.navigate("$navChat/${it.docId}") },
-                                onCancelClick = { navController.navigate(navHome) }
-                            )
-                        }
-
-                    }
+                        startDestination = if (isUserSignedIn) navHome else navSignIn,
+                        signInViewModel = get(),
+                        homeViewModel = get(),
+                        chatViewModel = get(),
+                        searchViewModel = get(),
+                    )
                 }
             }
         }
---
+++ app/src/main/java/com/joshgm3z/ping/PingAppContainer.kt
@@ -0,0 +1,76 @@
+package com.joshgm3z.ping
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.joshgm3z.ping.ui.chat.ChatScreenContainer
+import com.joshgm3z.ping.ui.frx.SignInContainer
+import com.joshgm3z.ping.ui.home.HomeScreenContainer
+import com.joshgm3z.ping.ui.search.SearchContainer
+import com.joshgm3z.ping.utils.Logger
+import com.joshgm3z.ping.viewmodels.ChatViewModel
+import com.joshgm3z.ping.viewmodels.HomeViewModel
+import com.joshgm3z.ping.viewmodels.SearchViewModel
+import com.joshgm3z.ping.viewmodels.SignInViewModel
+
+const val navSignIn = "signin_screen"
+const val navHome = "home_screen"
+const val navSearch = "search_screen"
+const val navChat = "chat_screen"
+
+@Composable
+fun PingAppContainer(
+    navController: NavHostController,
+    startDestination: String,
+    signInViewModel: SignInViewModel,
+    homeViewModel: HomeViewModel,
+    chatViewModel: ChatViewModel,
+    searchViewModel: SearchViewModel,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+    ) {
+
+        composable(navSignIn) {
+            Logger.debug("signIn")
+            SignInContainer(
+                signInViewModel = signInViewModel,
+                goToHome = { navController.navigate(navHome) }
+            )
+        }
+
+        composable(navHome) {
+            Logger.debug("navHome")
+            HomeScreenContainer(
+                homeViewModel = homeViewModel,
+                onSearchClick = { navController.navigate(navSearch) },
+                onChatClick = { navController.navigate("$navChat/${it.otherGuy.docId}") },
+            )
+        }
+
+        composable("$navChat/{userId}") {
+            Logger.debug("navChat")
+            val userId = it.arguments?.getString("userId")
+            LaunchedEffect(key1 = userId) {
+                chatViewModel.setUser(userId!!)
+            }
+            ChatScreenContainer(
+                chatViewModel = chatViewModel,
+                onBackClick = { navController.navigate(navHome) }
+            )
+        }
+
+        composable(navSearch) {
+            Logger.debug("navSearch")
+            SearchContainer(
+                searchViewModel = searchViewModel,
+                onSearchItemClick = { navController.navigate("$navChat/${it.docId}") },
+                onCancelClick = { navController.navigate(navHome) }
+            )
+        }
+
+    }
+}
